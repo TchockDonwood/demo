c9bda0e4dac6def6106bce05eabbd4e7b78811d0
TchockDonwood <savilovsem@gmail.com>
2026-02-20 22:16:31 +0500

    Added bootstrap

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -3,12 +3,14 @@ package com.example.demo.service;
 import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
 public class PostService {
     public Post[] listAllPosts() {
-        Post first = new Post("1");
-        Post second = new Post("2");
-        Post third = new Post("3");
+        Post first = new Post("Первое правило Бойцовского клуба – не упоминать о Бойцовском клубе.", new Date());
+        Post second = new Post("Второе правило Бойцовского клуба – не упоминать нигде о Бойцовском клубе.", new Date());
+        Post third = new Post("Третье правило Бойцовского клуба – боец крикнул «стоп», выдохся, отключился — бой окончен.", new Date());
 
         return new Post[] {first, second, third};
     }
